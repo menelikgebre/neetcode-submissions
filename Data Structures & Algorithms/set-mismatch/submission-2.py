68f78e4964767942195e1c88571c7eeb9ecb0177
menelikgebre <151406323+menelikgebre@users.noreply.github.com>
2026-06-27 11:19:28 -0400

    Add: set-mismatch - submission-2

---
+++ Data Structures & Algorithms/set-mismatch/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        # find one duplicate
+        # return duplicate value and missing value
+
+        # duplicate, just track w/ set and return dip
+        # get missing value
+            # set(1 - n) - set(seen)
+        
+        # what if just want to go until dup found and return
+
+        res = []
+        seen = set()
+        total = set(range(1, len(nums) + 1))
+        for num in nums:
+            if num in seen:
+                res.append(num)
+                continue
+            seen.add(num)
+        res.extend(total - seen)
+
+        return res
+            
+        
